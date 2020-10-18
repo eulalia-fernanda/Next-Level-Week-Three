fb954196a788b10baaeff4f5bc3a51c810081c49
eulalia-fernanda <eulalia.fend@gmail.com>
2020-10-18 18:58:30 -0300

    Ultima alterações

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -1,46 +1,34 @@
-module.exports = [
-    {
-        id: 1,
-        lat: "-23.584314",
-        lng: "-48.0469953",
-        name: "lar das meninas",
-        description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
-        images: [
-
-            "https://images.unsplash.com/photo-1574647267419-cd3adf200aed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-
-            "https://images.unsplash.com/photo-1598135753053-2e3075f90763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-
-            "https://images.unsplash.com/photo-1567701554261-fcc4bda64847?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
-        ],
-        instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
-
-        opening_hours: "Horários de visitas Das 18h até 8h",
-        open_on_weekends: "1"
-
-
-
-
-    },
-
-    {
-        id: 2,
-        lat: "-23.584314",
-        lng: "-48.0569953",
-        name: "lar dos meninos",
-        description:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
-
-        images: [
-
-            "https://images.unsplash.com/photo-1574647267419-cd3adf200aed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-
-            "https://images.unsplash.com/photo-1598135753053-2e3075f90763?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-
-            "https://images.unsplash.com/photo-1567701554261-fcc4bda64847?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
-        ],
-        instruction: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
-
-        opening_hours: "Horários de visitas Das 18h até 8h",
-        open_on_weekends: "0"
-  },
-]
+        module.exports = [
+            {
+            id: 1,
+            lat: "-23.584314",
+            lng: "-48.0469953",
+            name: "Lar das meninas",
+            description:
+                "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+            images: [
+                "https://images.unsplash.com/photo-1563465814437-b1a057a461ed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+                "https://images.unsplash.com/photo-1594575111057-47b35c5f98f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            ],
+            instructions:
+                "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+            opening_hours: "Horário de visitas Das 18h até 8h",
+            open_on_weekends: "1",
+            },
+            {
+            id: 2,
+            lat: "-23.584314",
+            lng:"-48.0469953",
+            name: "Lar dos meninos",
+            description:
+                "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+            images: [
+                "https://images.unsplash.com/photo-1563465814437-b1a057a461ed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+                "https://images.unsplash.com/photo-1594575111057-47b35c5f98f7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            ],
+            instructions:
+                "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+            opening_hours: "Horário de visitas Das 18h até 8h",
+            open_on_weekends: "0",
+            },
+        ];
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // importar dependencia
 const express = require('express');
 const path = require('path');
+const saveOrphanage = require('./database/saveOrphanage.js');
 const pages = require('./pages.js');
 
 //iniciando express
